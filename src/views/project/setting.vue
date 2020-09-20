e874076ebf62a679dd995d503083515454f72166
jimmy <pinbor@iissy.com>
2020-09-20 15:51:01 +0800

    layout

--- src/views/project/setting.vue
+++ src/views/project/setting.vue
@@ -4,7 +4,7 @@
       <Menu tagIndex="82"></Menu>
       <div class="rightMain">
         <Header title="项目配置中心"></Header>
-        <div class="app-main-container">
+        <div class="project-main-container">
           <div class="app-main-no-border">
             <div style="display: flex;flex-direction: row;height: 100%;">
               <Control :tagIndex="tagIndex" @tag_switch="tag_switch"></Control>
@@ -74,3 +74,7 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.project-main-container { height: 100%;overflow: auto;-webkit-flex: 1 1 auto;flex: 1 1 auto;width: auto;min-width: 0;display: -webkit-flex;display: flex;-webkit-flex-direction: column;flex-direction: column; }
+</style>
