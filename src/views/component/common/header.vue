2a716cfa840930e43318bb3e9d55bb31cca3ccb8
jimmy <pinbor@iissy.com>
2020-09-27 07:00:17 +0800

    layout

--- src/views/component/common/header.vue
+++ src/views/component/common/header.vue
@@ -1,23 +1,21 @@
 <template>
   <div class="app-header">
-    <div style="background-color: #ffffff;height:48px;padding:10px 10px 10px 20px;display: flex;flex-direction: row;width: 100%;">
-      <div style="flex: 1;flex-direction: column;align-items: center;justify-content: center;">
-        <div style="flex:1;vertical-align: middle;align-items: center;height: 100%;display: flex;">
-          <div style="font-size: 18px;font-weight: 500;color:#303030;flex: 0 0 auto;">天才团队 | {{title}}</div>
+    <div class="flex-row" style="width: 100%;">
+      <div style="background-color: #ffffff;height:48px;padding:10px 10px 10px 20px;display: flex;flex-direction: row;width: 100%;flex: 1;">
+        <div style="flex: 1;flex-direction: column;align-items: center;justify-content: center;">
+          <div style="flex:1;vertical-align: middle;align-items: center;height: 100%;display: flex;">
+            <div style="font-size: 18px;font-weight: 500;color:#303030;flex: 0 0 auto;">天才团队 | {{title}}</div>
+          </div>
         </div>
       </div>
       <div style="flex: 0;">
-        <div class="wrapper" ref="mbMenu">
-          <div class='nav-item' @click='dropdown'>
-            <div class='active dropdownBtn'>我的账号</div>
-          </div>
-        </div>
-        <div>
-          <ul class="dropdownWrapper" v-show='dropdownActive'>
-            <li><router-link to="/main/center/account">个人中心</router-link></li>
-            <li><router-link to="/main/logout">退出登录</router-link></li>
-          </ul>
-        </div>
+        <b-dropdown size="lg" right variant="link" toggle-class="text-decoration-none" offset="-10" no-caret>
+          <template v-slot:button-content>
+            <img src="/touxiang.jpg" style="height: 30px;width: 30px;border-radius: 30px;">
+          </template>
+          <b-dropdown-item cent href="/main/center/account">个人中心</b-dropdown-item>
+          <b-dropdown-item href="/main/logout">退出登录</b-dropdown-item>
+        </b-dropdown>
       </div>
     </div>
   </div>
@@ -58,13 +56,13 @@ export default {
 </script>
 
 <style scoped>
-.app-header .wrapper { display: flex; justify-content: center; width: 100px;border-radius: 20%!important;flex: 0;height: 100%; }
-/*.wrapper>div { flex: 1; text-align: center; }*/
-ul.dropdownWrapper { padding: 5px 0 5px 0;margin: 0;list-style-type: none; }
-.dropdownWrapper { border: 1px solid #eeeeee; font-size: 14px;background-color: #ffffff;display: flex;flex-direction: column;}
-.dropdownWrapper li { width: 100%;text-align: center;padding: 5px 0 5px 0; }
-.dropdownWrapper li:hover{ background-color: #eee; }
-.nav-item { cursor: pointer;display: flex;display: flex;display: -webkit-flex;flex-direction: column;flex: 1;align-items:center;justify-content:center; }
-.nav-item .active { font-size: 14px;text-align: center; }
-.dropdownBtn { display: inline-block; width: 100%; flex: 0 0 auto; }
+.app-header .wrapper { display: flex; justify-content: center; flex: 0;height: 100%;margin-right: 10px; }
+/*!*.wrapper>div { flex: 1; text-align: center; }*!*/
+/*ul.dropdownWrapper { padding: 5px 0 5px 0;margin: 0;list-style-type: none; }*/
+/*.dropdownWrapper { border: 1px solid #eeeeee; font-size: 14px;background-color: #ffffff;display: flex;flex-direction: column;}*/
+/*.dropdownWrapper li { width: 100%;text-align: center;padding: 5px 0 5px 0; }*/
+/*.dropdownWrapper li:hover{ background-color: #eee; }*/
+/*.nav-item { display: flex;display: flex;display: -webkit-flex;flex-direction: column;flex: 1;align-items:center;justify-content:center; }*/
+/*.nav-item .active { font-size: 14px;text-align: center; }*/
+/*.dropdownBtn { display: inline-block; flex: 0 0 auto;border-radius: 30px;overflow: hidden;cursor: pointer; }*/
 </style>
