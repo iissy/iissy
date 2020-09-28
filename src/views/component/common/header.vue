2aef51da4c94a4cc36003129c9f5490b97cad74f
jimmy <pinbor@iissy.com>
2020-09-28 17:23:04 +0800

    layout

--- src/views/component/common/header.vue
+++ src/views/component/common/header.vue
@@ -4,7 +4,14 @@
       <div style="background-color: #ffffff;height:48px;padding:10px 10px 10px 20px;display: flex;flex-direction: row;flex: 0 0 auto;">
         <div style="flex: 1;flex-direction: column;align-items: center;justify-content: center;">
           <div style="flex:1;vertical-align: middle;align-items: center;height: 100%;display: flex;">
-            <div style="font-size: 18px;font-weight: 500;color:#303030;flex: 0 0 auto;">{{title}}</div>
+            <div style="font-weight: 500;color:#303030;flex: 0 0 auto;" class="flex-row">
+              <div style="flex: 0 0 auto;">
+                <svg t="1601284538875" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="3757" width="24" height="24"><path d="M990.08 111.36a32 32 0 0 0-32 32V960H140.16a32 32 0 0 0 0 64h849.92a32 32 0 0 0 32-32V143.36a32 32 0 0 0-32-32z" p-id="3758" fill="#17C4BB"></path><path d="M393.6 841.6a32 32 0 0 0-32-32H64V64h745.6v300.8a32 32 0 0 0 64 0V32a32 32 0 0 0-32-32H32A32 32 0 0 0 0 32v809.6a32 32 0 0 0 32 32h329.6a32 32 0 0 0 32-32z" p-id="3759" fill="#17C4BB"></path><path d="M625.28 841.6a32 32 0 0 0 32 32H841.6a32 32 0 0 0 32-32V657.92a32 32 0 1 0-64 0v106.24L256 210.56h106.24a32 32 0 0 0 0-64H178.56a32 32 0 0 0-32 32v184.96a32 32 0 0 0 64 0V256l553.6 553.6H657.28a32 32 0 0 0-32 32z" p-id="3760" fill="#17C4BB"></path></svg>
+              </div>
+              <div style="flex: 0 0 auto;font-size: 18px;margin: 0 10px 0 5px;">
+                {{title}}
+              </div>
+            </div>
           </div>
         </div>
       </div>
