8e76ba52527193e0702be627c585e3b130a73c2a
jimmy <pinbor@iissy.com>
2020-10-16 12:19:31 +0800

    layout

--- src/views/component/common/block/alert.vue
+++ src/views/component/common/block/alert.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="iissy" class="alert flex-column">
+  <div id="iissy" class="alert flex-column align-items-center">
     <div style="align-items: center;flex: 0 0 auto;">
       <b-alert :show="dismissCountDown" :variant="variant" @dismissed="dismissCountDown=0">
-        <div class="flex-row" style="color: inherit;">
+        <div class="flex-row align-items-center" style="color: inherit;">
           <div class="mb-0">
             <b-iconstack font-scale="2">
               <b-icon stacked icon="check-circle" :variant="variant"></b-icon>
@@ -23,9 +23,6 @@ export default {
       variant: ''
     }
   },
-  props: {
-    showAlert: Boolean
-  },
   methods: {
     success: function () {
       this.dismissCountDown = 1;
@@ -40,6 +37,6 @@ export default {
 </script>
 
 <style scoped>
-#iissy.alert { position:fixed;top: 100px;left: 0;right: 0;margin:0px auto;text-align:center;height: 0px;border: none;padding: 0; }
-#iissy .alert { border-radius: 15px;padding: 5px 10px 5px 10px; }
+#iissy.alert { position:fixed;top: 100px;left: 0;right: 0;margin:0px auto;text-align:center;height: 0px;border: none;padding: 0;opacity: 0.8;z-index: 999; }
+#iissy .alert { border-radius: 10px;padding: 5px 10px 5px 5px; }
 </style>
