fa448641b52fa05eb8811646ac2f7aa6cc36222a
jimmy <pinbor@iissy.com>
2020-10-20 09:14:03 +0800

    layout

--- src/views/component/attr/project_info.vue
+++ src/views/component/attr/project_info.vue
@@ -1,16 +1,73 @@
 <template>
-  <div>
-    asdfasdfggggggggggggggggggggg4
+  <div class="app-main-content">
+    <div style="font-size: 18px;margin-bottom: 20px;border-bottom: 1px solid #e8e8e8;padding: 20px 0 10px 20px;">项目信息</div>
+    <div style="padding: 0 10px;">
+      <b-container fluid>
+        <b-row style="margin-bottom: 20px;">
+          <b-col sm="1">
+            <label>创建者</label>
+          </b-col>
+          <b-col sm="5">
+            <Creator :user="creator"/>
+          </b-col>
+        </b-row>
+        <b-row style="margin-bottom: 20px;">
+          <b-col sm="1">
+            <label>创建时间</label>
+          </b-col>
+          <b-col sm="5">
+            2019-01-01 12:12:12
+          </b-col>
+        </b-row>
+        <b-row style="margin-bottom: 20px;">
+          <b-col sm="1">
+            <label>项目名称</label>
+          </b-col>
+          <b-col sm="5">
+            <b-form-input @change="name_change" @keydown="name_change" v-model="name"></b-form-input>
+          </b-col>
+        </b-row>
+        <b-row style="margin-bottom: 20px;">
+          <b-col sm="1">
+            <label></label>
+          </b-col>
+          <b-col sm="5">
+            <AddProjectButton :disabled="disabled" title="更新信息" @submit="update"></AddProjectButton>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+    <Alert ref="alert"></Alert>
   </div>
 </template>
 
 <script>
+import Creator from '@/views/component/task/creator';
+import AddProjectButton from '@/views/component/button/common';
+import Alert from '@/views/component/common/block/alert';
+
 export default {
   data() {
     return {
+      name: '',
+      disabled: true,
+      creator: { uuid: 'pinbor', name: 'jimmy', email: 'pinbor@iissy.com' }
     }
   },
   methods: {
+    name_change: function () {
+      let self = this;
+      self.disabled = false;
+    },
+    update: function () {
+      let self = this;
+      self.$refs.alert.success();
+    }
+  },
+  components: {
+    Creator,
+    AddProjectButton,
+    Alert
   }
 }
 </script>
---
+++ src/views/component/task/creator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex-row">
+    <div style="flex: 0 0 auto;">
+      <img src="/touxiang.jpg" style="height: 20px;width: 20px;border-radius: 20px;">
+    </div>
+    <div style="flex: 0 0 auto;margin-left: 10px;">
+      <div style="text-align: left;color: #363636;">{{ user.name }} ({{ user.email }})</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  props: {
+    user: Object
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
