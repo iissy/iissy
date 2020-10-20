05ae574680333563cf696bf1bc8cf3b030a24a59
jimmy <pinbor@iissy.com>
2020-10-20 14:59:01 +0800

    layout

--- src/views/component/common/block/i_b_icon.vue
+++ src/views/component/common/block/i_b_icon.vue
@@ -2,19 +2,19 @@
   <div style="flex: 0 0 auto;" class="align-items-center">
     <div v-if="icon==='1'" style="display: flex;">
       <div class="flex-column align-items-center">
-        <div style="flex: 1;"><b-icon icon="briefcase-fill"/></div>
+        <div style="flex: 1;color: #1677FF;"><b-icon icon="briefcase-fill"/></div>
         <div style="flex: 1;">&nbsp;</div>
       </div>
     </div>
     <div v-if="icon==='2'" style="display: flex;">
       <div class="flex-column align-items-center">
-        <div style="flex: 1;"><b-icon icon="bug-fill"/></div>
+        <div style="flex: 1;color: #F7B621;"><b-icon icon="bug-fill"/></div>
         <div style="flex: 1;">&nbsp;</div>
       </div>
     </div>
     <div v-if="icon==='7'" style="display: flex;">
       <div class="flex-column align-items-center">
-        <div style="flex: 1;"><b-icon icon="diagram3-fill"/></div>
+        <div style="flex: 1;color: #3FD698;"><b-icon icon="receipt"/></div>
         <div style="flex: 1;">&nbsp;</div>
       </div>
     </div>
