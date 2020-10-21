37e016a2c86f4100c65b31bb45c02cfee427d961
jimmy <pinbor@iissy.com>
2020-10-21 08:50:54 +0800

    layout

--- src/views/component/setting/issue_type/field.vue
+++ src/views/component/setting/issue_type/field.vue
@@ -17,7 +17,7 @@
             <div class="th">属性名称</div>
             <div class="th">属性类型</div>
             <div class="th">使用到的项目</div>
-            <div class="th last">操作</div>
+            <div class="th op justify-content-center align-items-center" style="display: flex;">操作</div>
           </div>
           <div class="table-row" v-for="item in items" v-bind:key="item.uuid">
             <div class="td flex-row">
@@ -28,12 +28,12 @@
             </div>
             <div class="td">{{ item.type }}</div>
             <div class="td">所有项目</div>
-            <div class="td last" style="display: flex;align-items: center;">
+            <div class="td op justify-content-center align-items-center" style="display: flex;">
               <div style="width: 30px;height: 2px;" v-if="item.built_in">
                 <div style="width: 100%;height: 100%;background-color: #e0e0e0;"></div>
               </div>
-              <div v-else>
-                <svg t="1600587759121" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="5114" width="16" height="16"><path d="M867.858 286.276c0 0 29.859-23.187-1.104-54.1l-91.687-95.672c0 0-27.363-37.252-59.429-5.235l-61.013 65.621 153.853 149.917 59.38-60.532zM773.818 387.109l-162.877-154.069-369.823 372.031 166.765 143.317 365.934-361.278zM221.15 845.308l-62.309-57.579-15.595 68.835h-0.054v49.156h737.34v-49.156h-735.708l76.327-11.256zM360.073 788.686l-164.126-137.483-20.834 92.334 95.287 80.528 89.672-35.378z" p-id="5115" fill="#2c2c2c"></path></svg>
+              <div style="font-weight: bolder;" v-else>
+                <b-icon icon="x" scale="2"></b-icon>
               </div>
             </div>
           </div>
@@ -83,5 +83,5 @@ export default {
 .table-row:hover { background-color: #f8f8f8; }
 .table .th { width: 100px;padding: 10px 0 10px 20px;font-size: 12px;flex: 1 1 auto; }
 .table .td { width: 100px;padding: 10px 0 10px 20px;flex: 1 1 auto; }
-.table .last { flex: 0 0 auto; }
+.table .op { flex: 0 0 100px; }
 </style>
