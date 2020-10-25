c8af2b0a886e19947ae23fc1ffbf2cdeb66421a6
jimmy <pinbor@iissy.com>
2020-10-25 16:53:13 +0800

    layout

--- src/views/component/attr/project_field.vue
+++ src/views/component/attr/project_field.vue
@@ -2,7 +2,7 @@
   <div class="app-main-content" style="padding: 20px;">
     <div style="font-size: 18px;margin-bottom: 20px;">项目属性</div>
     <Summary :desc="desc"/>
-    <Search style="margin: 20px 0;" placeholder="项目属性" />
+    <Search style="margin: 20px 0;" placeholder="搜索项目属性" />
     <div id="project-main">
       <div style="-webkit-flex: 1;flex: 1;position: relative;z-index: 0;display: flex;">
         <div class="table">
