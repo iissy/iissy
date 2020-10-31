80219463b0412d9fcba4c614e4e29fb65ed46070
jimmy <pinbor@iissy.com>
2020-10-31 22:21:09 +0800

    layout

--- src/views/project/list.vue
+++ src/views/project/list.vue
@@ -3,58 +3,56 @@
     <div class="app-row">
       <Menu tagIndex="21" ref="Menu"></Menu>
       <div class="rightMain">
-        <div class="layout-container-column">
-          <Header title="项目管理" ref="Header"></Header>
-          <div class="app-main-container">
-            <div class="right-container">
-              <div class="right-container-inner">
-                <div id="project-top-row">
-                  <div style="flex: 0 1 auto;">
-                    <div class="tab-title">
-                      <div style="flex:0 0 auto;text-align: center;cursor: pointer;padding: 0 10px 10px 10px;" v-for="(title,index) in tabTitle" @click="cur=index" :class="{active:cur==index}" :key="index">{{title}}</div>
-                    </div>
-                  </div>
-                  <div style="flex: 1;">&nbsp;</div>
-                  <div style="flex: 0 0 auto;" class="flex-row">
-                    <div style="flex: 0 0 auto;">
-                      <svg t="1601475264212" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="30270" width="14" height="14"><path d="M958.464 385.024 819.2 385.024 917.504 286.72c24.576-24.576 24.576-65.536 0-90.112l-90.112-90.112c-24.576-24.576-65.536-24.576-90.112 0L638.976 204.8 638.976 65.536c0-36.864-28.672-65.536-65.536-65.536l-126.976 0c-36.864 0-65.536 28.672-65.536 65.536L380.928 204.8 286.72 106.496c-24.576-24.576-65.536-24.576-90.112 0L106.496 196.608C77.824 221.184 77.824 262.144 106.496 286.72L204.8 385.024 65.536 385.024C28.672 385.024 0 413.696 0 446.464l0 126.976c0 36.864 28.672 65.536 65.536 65.536L204.8 638.976 106.496 737.28c-24.576 24.576-24.576 65.536 0 90.112l90.112 90.112c24.576 24.576 65.536 24.576 90.112 0L385.024 819.2l0 139.264c0 36.864 28.672 65.536 65.536 65.536l126.976 0c36.864 0 65.536-28.672 65.536-65.536L643.072 819.2l98.304 98.304c24.576 24.576 65.536 24.576 90.112 0l90.112-90.112c24.576-24.576 24.576-65.536 0-90.112L819.2 638.976l139.264 0c36.864 0 65.536-28.672 65.536-65.536l0-126.976C1024 413.696 995.328 385.024 958.464 385.024zM512 704.512c-106.496 0-192.512-86.016-192.512-192.512 0-106.496 86.016-192.512 192.512-192.512 106.496 0 192.512 86.016 192.512 192.512C704.512 618.496 618.496 704.512 512 704.512z" p-id="30271" fill="#707070"></path></svg>
-                    </div>
-                    <div style="margin-right: 20px;margin-left: 5px;">项目配置中心</div>
+        <Header title="项目管理" ref="Header"></Header>
+        <div class="app-main-container">
+          <div class="right-container">
+            <div class="right-container-inner">
+              <div id="project-top-row">
+                <div style="flex: 0 1 auto;">
+                  <div class="tab-title">
+                    <div style="flex:0 0 auto;text-align: center;cursor: pointer;padding: 0 10px 10px 10px;" v-for="(title,index) in tabTitle" @click="cur=index" :class="{active:cur==index}" :key="index">{{title}}</div>
                   </div>
                 </div>
-                <div id="project-new-row">
-                  <div style="flex: 0 0 auto;flex-direction: row;margin-left: 10px;text-align: left;">
-                    <AddProjectButton title="新增项目" @submit="add"></AddProjectButton>
+                <div style="flex: 1;">&nbsp;</div>
+                <div style="flex: 0 0 auto;" class="flex-row">
+                  <div style="flex: 0 0 auto;">
+                    <svg t="1601475264212" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="30270" width="14" height="14"><path d="M958.464 385.024 819.2 385.024 917.504 286.72c24.576-24.576 24.576-65.536 0-90.112l-90.112-90.112c-24.576-24.576-65.536-24.576-90.112 0L638.976 204.8 638.976 65.536c0-36.864-28.672-65.536-65.536-65.536l-126.976 0c-36.864 0-65.536 28.672-65.536 65.536L380.928 204.8 286.72 106.496c-24.576-24.576-65.536-24.576-90.112 0L106.496 196.608C77.824 221.184 77.824 262.144 106.496 286.72L204.8 385.024 65.536 385.024C28.672 385.024 0 413.696 0 446.464l0 126.976c0 36.864 28.672 65.536 65.536 65.536L204.8 638.976 106.496 737.28c-24.576 24.576-24.576 65.536 0 90.112l90.112 90.112c24.576 24.576 65.536 24.576 90.112 0L385.024 819.2l0 139.264c0 36.864 28.672 65.536 65.536 65.536l126.976 0c36.864 0 65.536-28.672 65.536-65.536L643.072 819.2l98.304 98.304c24.576 24.576 65.536 24.576 90.112 0l90.112-90.112c24.576-24.576 24.576-65.536 0-90.112L819.2 638.976l139.264 0c36.864 0 65.536-28.672 65.536-65.536l0-126.976C1024 413.696 995.328 385.024 958.464 385.024zM512 704.512c-106.496 0-192.512-86.016-192.512-192.512 0-106.496 86.016-192.512 192.512-192.512 106.496 0 192.512 86.016 192.512 192.512C704.512 618.496 618.496 704.512 512 704.512z" p-id="30271" fill="#707070"></path></svg>
                   </div>
-                  <div style="flex: 1;display: inline-block;padding-right: 20px;"></div>
-                  <div style="flex: 0 0 auto;flex-direction: column;align-items: center;display: flex;">
-                    <div style="width: 100%;text-align: right;flex: 1;align-items: center;display: flex;">
-                      <div style="flex: 1;display: inline-block;padding-right: 10px;">
-                        <Search placeholder="项目名字" />
-                      </div>
+                  <div style="margin-right: 20px;margin-left: 5px;">项目配置中心</div>
+                </div>
+              </div>
+              <div id="project-new-row">
+                <div style="flex: 0 0 auto;flex-direction: row;margin-left: 10px;text-align: left;">
+                  <AddProjectButton title="新增项目" @submit="add"></AddProjectButton>
+                </div>
+                <div style="flex: 1;display: inline-block;padding-right: 20px;"></div>
+                <div style="flex: 0 0 auto;flex-direction: column;align-items: center;display: flex;">
+                  <div style="width: 100%;text-align: right;flex: 1;align-items: center;display: flex;">
+                    <div style="flex: 1;display: inline-block;padding-right: 10px;">
+                      <Search placeholder="项目名字" />
                     </div>
                   </div>
                 </div>
-                <div id="project-main">
-                  <div style="-webkit-flex: 1;flex: 1;position: relative;z-index: 0;display: flex;overflow-x: auto;padding: 10px;">
-                    <b-table :fields="fields" :items="items" bordered striped>
-                      <template v-slot:cell(nameuuid)="data">
-                        <router-link :to="{ name:'Redirect', params: { team: team, project: data.item.uuid } }">{{ data.item.name }}</router-link>
-                      </template>
-                      <template v-slot:cell(status)="data">
-                        <Status :name="data.item.status_category" :color="data.item.status_uuid" />
-                      </template>
-                      <template v-slot:cell(owner)>
-                        <b-progress :value="25" variant="success" striped animate></b-progress>
-                      </template>
-                      <template v-slot:cell(assign)="data">
-                        <User :user="data.item.assign" :hasEmail="hasEmail"/>
-                      </template>
-                      <template v-slot:cell(create_time)="data">
-                        {{ data.value | formatDate }}
-                      </template>
-                    </b-table>
-                  </div>
+              </div>
+              <div id="project-main">
+                <div style="-webkit-flex: 1;flex: 1;position: relative;z-index: 0;display: flex;overflow-x: auto;padding: 10px;">
+                  <b-table :fields="fields" :items="items" bordered striped>
+                    <template v-slot:cell(nameuuid)="data">
+                      <router-link :to="{ name:'Redirect', params: { team: team, project: data.item.uuid } }">{{ data.item.name }}</router-link>
+                    </template>
+                    <template v-slot:cell(status)="data">
+                      <Status :name="data.item.status_category" :color="data.item.status_uuid" />
+                    </template>
+                    <template v-slot:cell(owner)>
+                      <b-progress :value="25" variant="success" striped animate></b-progress>
+                    </template>
+                    <template v-slot:cell(assign)="data">
+                      <User :user="data.item.assign" :hasEmail="hasEmail"/>
+                    </template>
+                    <template v-slot:cell(create_time)="data">
+                      {{ data.value | formatDate }}
+                    </template>
+                  </b-table>
                 </div>
               </div>
             </div>
