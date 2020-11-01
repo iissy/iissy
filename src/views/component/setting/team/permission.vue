0731b428b26c84a51cd1f22cf25a6510a7f65fba
jimmy <pinbor@iissy.com>
2020-11-01 19:14:58 +0800

    layout

--- src/views/component/setting/team/permission.vue
+++ src/views/component/setting/team/permission.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="max-width:1200px;background-color: #ffffff;flex: 1 1 auto;padding: 20px;height: 100%;box-shadow: 0 4px 6px 0 rgba(31,31,31,0.05), 0 0 2px 0 rgba(31,31,31,0.2);">
+  <div style="max-width:1200px;flex: 1 1 auto;padding: 20px;height: 100%;" class="g-container  flex-column">
     <div style="font-size: 18px;">团队权限</div>
   </div>
 </template>
