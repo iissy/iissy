1213a2523688828adf00b7e392fee06e3826de8f
jimmy <pinbor@iissy.com>
2020-11-03 16:22:45 +0800

    layout

--- src/views/component/com/com00005.vue
+++ src/views/component/com/com00005.vue
@@ -28,28 +28,28 @@
         </div>
       </div>
       <div style="flex: 1;margin: 10px 20px;" class="flex-column">
-        <div class="report-item">常用报表</div>
+        <div class="report-item report-title">常用报表</div>
         <div class="report-item flex-row">
-          <div style="flex: 0 0 auto;color: #533f03;">
+          <div style="flex: 0 0 auto;color: #CD950C;">
             <b-icon icon="bar-chart-line" scale="1.2"/>
           </div>
-          <div style="flex: 0 0 auto;margin-left: 10px;">
+          <div style="flex: 0 0 auto;margin-left: 15px;">
             需求状态分布
           </div>
         </div>
         <div class="report-item flex-row">
-          <div style="flex: 0 0 auto;color: #a71d2a;">
+          <div style="flex: 0 0 auto;color: #A020F0;">
             <b-icon icon="bar-chart-line" scale="1.2"/>
           </div>
-          <div style="flex: 0 0 auto;margin-left: 10px;">
+          <div style="flex: 0 0 auto;margin-left: 15px;">
             缺陷状态分布
           </div>
         </div>
         <div class="report-item flex-row">
-          <div style="flex: 0 0 auto;color: #491217;">
-            <b-icon icon="bar-chart-steps" scale="1.2"/>
+          <div style="flex: 0 0 auto;color: #CD1076;">
+            <b-icon icon="bar-chart-line" scale="1.2"/>
           </div>
-          <div style="flex: 0 0 auto;margin-left: 10px;">
+          <div style="flex: 0 0 auto;margin-left: 15px;">
             任务状态分布
           </div>
         </div>
@@ -57,7 +57,7 @@
           <div style="flex: 0 0 auto;color: #1e7e34;">
             <b-icon icon="bar-chart-line" scale="1.2"/>
           </div>
-          <div style="flex: 0 0 auto;margin-left: 10px;">
+          <div style="flex: 0 0 auto;margin-left: 15px;">
             每日工时日志报表
           </div>
         </div>
@@ -86,8 +86,8 @@ export default {
 </script>
 
 <style scoped>
+.report .report-title { font-size: 16px; }
 .report .report-menu { border-bottom: 1px solid #e8e8e8;line-height: 48px;padding-left: 20px; }
 .report .report-menu.active { border-left: 3px solid #17C4BB;padding-left: 17px; }
-.report-item { font-size: 16px;border-bottom: 1px solid #e8e8e8;padding: 10px 0; }
-.report-item div { font-size: 16px; }
+.report-item { border-bottom: 1px solid #e8e8e8;padding: 10px 0; }
 </style>
