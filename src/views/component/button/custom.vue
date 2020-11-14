a69d4f050a5a5f30c4f66b42b254b66b3dada4b0
jimmy <pinbor@iissy.com>
2020-11-14 09:32:33 +0800

    layout

---
+++ src/views/component/button/custom.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="display: inline-block;box-shadow: none;outline: none;">
+    <div  style="box-shadow: none;outline: none;" class="button" :class="{disabled: disabled, fill: fill}" v-on:click="onsubmit">{{title}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+    };
+  },
+  props: {
+    title: String,
+    disabled: {
+      default: false
+    },
+    fill: {
+      default: true
+    }
+  },
+  methods: {
+    onsubmit: function () {
+      let self = this;
+      if (!self.disabled) {
+        this.$emit('submit');
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.button { padding: 0 15px;font-size:14px;border-radius:3px;cursor: pointer;border: 1px solid #dedede;line-height: 24px; }
+.disabled { opacity: .65; }
+.fill { background-color: #36c6d3;color:#ffffff;border: 1px solid #36c6d3; }
+</style>
--- src/views/component/wiki/page/add.vue
+++ src/views/component/wiki/page/add.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>asdfasdf</div>
+  <div class="flex-column justify-content-center" style="flex: 1;">
+    <div style="flex: 0 0 40px;padding-left: 20px;background-color: #f8f8f8;border-bottom: 1px solid #dedede;" class="flex-row align-items-center">
+      <div style="flex: 1;">
+        asdf
+      </div>
+      <div style="flex: 0 0 auto;margin-right: 20px;">
+        <AddAndBackButton title="返回" :fill="fill"/>
+        <AddAndBackButton style="margin-left: 10px;" title="发布"/>
+      </div>
+    </div>
+    <div style="flex: 1;">
+      asdf
+    </div>
+  </div>
 </template>
 
 <script>
+import AddAndBackButton from '@/views/component/button/custom';
+
 export default {
-name: "add"
+  data() {
+    return {
+      fill: false
+    }
+  },
+  components: {
+    AddAndBackButton
+  }
 }
 </script>
 
