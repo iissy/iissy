466ec3923184a2cb6e39711c7b462c720a733209
jimmy <pinbor@iissy.com>
2020-11-19 14:36:34 +0800

    layout

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,18 +1,4 @@
 <template>
-<!--  <div style="margin: 0 auto 0 auto;width: 100%;height: 100%;" class="flex-column justify-content-center">-->
-<!--    <div style="flex: 0 0 48px;"></div>-->
-<!--    <div class="enter" style="padding-top: 10px;background-color: #ffffff;flex: 1;">-->
-<!--      <router-link :to="{ name: 'MainRedirect' }">进入 soul 项目管理系统</router-link>-->
-<!--    </div>-->
-<!--    <div style="flex: 0 0 60px;" class="flex-row align-items-center justify-content-center copyright">-->
-<!--      <div>-->
-<!--        Copyright ©2020 <a href="/">爱斯园</a>-->
-<!--      </div>-->
-<!--      <div style="margin-left: 5px;">-->
-<!--        <a href="http://www.miibeian.gov.cn" target="_blank">粤ICP备2020117325号</a>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
   <div style="width: 100%;height: 100%;" class="flex-row align-items-center justify-content-center">
     <div style="position: absolute;width: 800px;height: 600px;background-color: #f0f0f0;border-radius: 10px;overflow: hidden;" class="flex-row align-items-center justify-content-center">
       <div style="position: absolute;color: rgba(255, 206, 86, 0.2);margin:-420px 0 0 -200px;">
@@ -32,7 +18,7 @@
           <b-icon icon="arrow-left-short" scale="2"/>
         </div>
         <div style="margin-left: 5px;" class="flex-row align-items-center justify-content-center enter">
-          <router-link :to="{ name: 'MainRedirect' }">进入 soul 项目管理系统</router-link>
+          <router-link :to="{ name: 'MainRedirect' }">进入 Soul 项目管理系统</router-link>
         </div>
       </div>
       <div style="position: absolute;bottom: 10px;">
