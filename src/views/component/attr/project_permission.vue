c4da634c95c611cf41b422bcac7aa5b886ba8d1d
jimmy <pinbor@iissy.com>
2020-12-01 11:18:34 +0800

    layout

--- src/views/component/attr/project_permission.vue
+++ src/views/component/attr/project_permission.vue
@@ -4,21 +4,93 @@
       <div style="flex: 0 0 auto;font-size: 18px;">项目权限</div>
     </div>
     <Summary :desc="desc"/>
-    <PermissionItem title="查看项目" desc="允许成员浏览当前项目，包括工作项，迭代，筛选器等信息"/>
+    <PermissionItem v-if="loaded" :role_members="role_members" :data="data" :group="maps.browse"/>
+    <PermissionItem v-if="loaded" :role_members="role_members" :data="data" :group="maps.reports"/>
   </div>
 </template>
 
 <script>
 import Summary from '@/views/component/common/block/summary';
 import PermissionItem from '@/views/component/common/block/permission_item';
+import http from "@/scripts/http";
 
 export default {
   data() {
     return {
-      desc: '项目权限可以用于定制项目的操作权限控制。'
+      desc: '项目权限可以用于定制项目的操作权限控制。',
+      team: '',
+      project: '',
+      maps: {
+        browse: { code: 1102, permission: 'browse_project', title: '查看项目', desc: '允许成员浏览当前项目，包括工作项，筛选器，报表等信息', groups: [], roles: [], members: [] },
+        reports: { code: 1104, permission: 'view_project_reports', title: '查看项目报表', desc: '允许成员查看当前项目的数据报表', groups: [], roles: [], members: [] }
+      },
+      role_members: [],
+      loaded: false
     }
   },
+  mounted() {
+    let self = this;
+    self.data = {
+      permission_rule: {
+        context_type: "project",
+        context_param: {
+          project_uuid: self.project
+        }
+      }
+    }
+  },
+  created: function () {
+    let self = this;
+    self.team = self.$route.params.team;
+    self.project = this.$route.params.project;
+    self.GetUserDomainGroups();
+  },
   methods: {
+    GetUserDomainGroups: function () {
+      let self = this;
+      let data = []
+      for(let key in self.maps) {
+        data.push(self.maps[key].code);
+      }
+      http.post(self.urls.project_user_domain_group.format(self.team, self.project), data).then(function (response) {
+        if(response.data && response.data.length > 0) {
+          for (let i = 0; i < response.data.length; i++) {
+            let rules = response.data[i];
+            for (let key in self.maps) {
+              let item = self.maps[key];
+              if (rules.permission === item.code) {
+                item.groups = rules.groups;
+                break;
+              }
+            }
+          }
+        }
+        self.GetRoleMembers();
+      });
+    },
+    GetRoleMembers: function () {
+      let self = this;
+      http.get(self.urls.role_members.format(self.team, self.project)).then(function (response) {
+        if(response.data && response.data.role_members && response.data.role_members.length > 0) {
+          for (let i = 0; i < response.data.role_members.length; i++) {
+            let role_member = response.data.role_members[i];
+            for (let key in self.maps) {
+              let item = self.maps[key];
+              item.roles = [];
+              item.members = [];
+              role_member.role.type = 'role';
+              item.roles.push(role_member.role);
+              for (let x=0;x<role_member.members.length;x++) {
+                let user = role_member.members[x];
+                user.type = 'single_user';
+                item.members.push(user);
+              }
+            }
+          }
+        }
+        self.loaded = true;
+      });
+    },
   },
   components: {
     Summary,
