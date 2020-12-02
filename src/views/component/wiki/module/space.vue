9d9430a18f40d9b53f50f85a7a8baf310deb67c1
jimmy <pinbor@iissy.com>
2020-12-02 18:33:18 +0800

    layout

--- src/views/component/wiki/module/space.vue
+++ src/views/component/wiki/module/space.vue
@@ -1,7 +1,7 @@
 <template>
   <div @click="goto" style="width: 250px;height: 175px;padding: 15px 20px 10px 20px;cursor: pointer;margin-right: 10px;margin-top: 10px;" class="g-container flex-column">
     <div style="flex: 1;">
-      <div style="flex: 0 0 auto;font-size: 16px;color: #333333;">{{ space.title }}</div>
+      <div style="flex: 0 0 auto;font-size: 16px;color: #333333;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;">{{ space.title }}</div>
       <div style="flex: 0 0 auto;color: #909090;font-size: 14px;">{{ desc }}</div>
     </div>
     <div style="flex: 0 0 auto;" class="flex-row">
