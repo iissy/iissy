4731490a052ef883256e5897bae420857c204e82
jimmy <pinbor@iissy.com>
2020-12-05 12:05:30 +0800

    layout

--- src/views/component/wiki/module/space.vue
+++ src/views/component/wiki/module/space.vue
@@ -1,8 +1,8 @@
 <template>
   <div @click="goto" style="width: 250px;height: 175px;padding: 15px 15px 10px 20px;cursor: pointer;margin-right: 10px;margin-top: 10px;" class="g-container flex-column">
     <div style="flex: 0 0 auto;">
-      <div style="flex: 0 0 auto;font-size: 16px;color: #333333;padding-right: 5px;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;" class="flex-row align-items-center">
-        <div style="flex: 1;">{{ space.title }}</div>
+      <div style="flex: 0 0 auto;color: #333333;padding-right: 5px;" class="flex-row align-items-center">
+        <div style="flex: 1;white-space: nowrap;text-overflow: ellipsis;overflow: hidden;font-size: 15px;">{{ space.title }}</div>
         <div style="flex: 0 0 auto;align-content: end;" class="pin-star">
           <b-icon @click="setPin" icon="star" scale="1.3"></b-icon>
         </div>
