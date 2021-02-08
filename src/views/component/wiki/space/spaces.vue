8b9ff7d069837ef34ecef4b10667729fd0aae8d8
jimmy <pinbor@iissy.com>
2021-02-08 14:30:15 +0800

    layout

--- src/views/component/wiki/space/spaces.vue
+++ src/views/component/wiki/space/spaces.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="overview" style="overflow:auto;flex: 1 0 auto;padding: 10px;width: 100%;position: relative;">
-    <div class="flex-row header-container-inner" style="border-radius: 3px;flex: 1;padding: 10px;">
+    <div class="flex-row header-container-inner" style="border-radius: 3px;flex: 1;padding: 10px;margin-bottom: 10px;">
       <div style="flex: 0 0 auto;">
         <AddButton title="新建页面组" v-b-modal.modal-wiki-add></AddButton>
-      </div>
-      <div style="flex: 1;"></div>
-      <div style="flex: 0 0 auto;" class="flex-row align-items-center">
-        <b-icon icon="gear" aria-hidden="true" scale="1.2"></b-icon>
-        <div style="margin: 0 20px 0 5px;">Wiki 配置中心</div>
       </div>
     </div>
     <div style="flex: 1;flex-direction: row;position: relative;" class="flex-row flex-wrap">
