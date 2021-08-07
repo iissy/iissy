a85e7ba5597f54d1745a01afb796bcbe6bd3fd7f
jimmy <pinbor@iissy.com>
2021-08-07 09:33:41 +0800

    侧边栏打开关闭状态保留

--- src/store/modules/app.js
+++ src/store/modules/app.js
@@ -1,6 +1,8 @@
+import Cookies from 'js-cookie'
+
 const state = {
     sidebar: {
-        opened: true,
+        opened: Cookies.get('sidebarStatus') ? !!+Cookies.get('sidebarStatus') : true,
         withoutAnimation: false
     }
 }
@@ -9,8 +11,14 @@ const mutations = {
     TOGGLE_SIDEBAR: state => {
         state.sidebar.opened = !state.sidebar.opened
         state.sidebar.withoutAnimation = false
+        if (state.sidebar.opened) {
+            Cookies.set('sidebarStatus', 1)
+        } else {
+            Cookies.set('sidebarStatus', 0)
+        }
     },
     CLOSE_SIDEBAR: (state, withoutAnimation) => {
+        Cookies.set('sidebarStatus', 0)
         state.sidebar.opened = false
         state.sidebar.withoutAnimation = withoutAnimation
     }
