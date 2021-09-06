4cc3110e59022248543e23a0422d99233282b0f3
jimmy <pinbor@iissy.com>
2021-09-06 17:00:34 +0800

    refactor: 优化

--- src/conf/codes.js
+++ src/conf/codes.js
@@ -1,7 +1,11 @@
 let codes = {
     ServerError: '服务错误',
     NotFound: '不存在',
-    User: '用户'
+    User: '用户',
+    AuthFailure: '授权失败',
+    InvalidParameter: '参数无效',
+    InvalidToken: '凭证无效',
+    MissingToken: '缺失凭证'
 };
 
 export default codes;
