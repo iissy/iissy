71fe4ce0643f83aa88aa592d224c15fd524d2215
jimmy <pinbor@iissy.com>
2021-09-21 13:33:36 +0800

    refactor: 优化

--- src/components/common/block/summary.vue
+++ src/components/common/block/summary.vue
@@ -1,5 +1,5 @@
 <template>
-  <div style="flex: 0 0 auto;align-items: center;" v-b-tooltip.hover :title="desc" tabindex="0">
+  <div v-if="desc" style="flex: 0 0 auto;align-items: center;" v-b-tooltip.hover :title="desc" tabindex="0">
     <b-icon icon="question-octagon" scale="1.2"></b-icon>
   </div>
 </template>
