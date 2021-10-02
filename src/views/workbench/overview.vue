4183da78e8ee87ee8b828826c05680605c016d0c
jimmy <pinbor@iissy.com>
2021-10-02 15:50:41 +0800

    refactor: 优化样式

--- src/views/workbench/overview.vue
+++ src/views/workbench/overview.vue
@@ -8,18 +8,22 @@
       <Dashboard name="项目仪表盘"/>
     </div>
 
-    <div class="flex-row b-line" style="padding: 0 20px 5px 0;margin: 20px 0 0 0;">
-      <div style="flex: 1;font-size: 18px;">项目 / 最近浏览</div>
-    </div>
-    <div style="flex: 1;flex-direction: row;position: relative;" class="flex-row flex-wrap">
-      <Project v-for="p in projects" :project="p" :key="p.uuid"/>
+    <div style="margin: 20px 0 0 0;">
+      <div class="flex-row b-line" style="padding: 0 20px 5px 0;">
+        <div style="flex: 1;font-size: 18px;">项目 / 最近浏览</div>
+      </div>
+      <div style="flex: 1;flex-direction: row;position: relative;" class="flex-row flex-wrap">
+        <Project v-for="p in projects" :project="p" :key="p.uuid"/>
+      </div>
     </div>
 
-    <div class="flex-row b-line" style="padding: 0 20px 5px 0;margin: 20px 0 0 0;">
-      <div style="flex: 1;font-size: 18px;">文档 / 最近浏览</div>
-    </div>
-    <div style="flex: 1;flex-direction: row;position: relative;" class="flex-row flex-wrap">
-      <Space v-for="space in spaces" :key="space.uuid" :space="space" desc="最近页面更新于 8 小时前"/>
+    <div v-if="loaded" style="margin: 20px 0 0 0;">
+      <div class="flex-row b-line" style="padding: 0 20px 5px 0;">
+        <div style="flex: 1;font-size: 18px;">文档 / 最近浏览</div>
+      </div>
+      <div style="flex: 1;flex-direction: row;position: relative;" class="flex-row flex-wrap">
+        <Space v-for="space in spaces" :key="space.uuid" :space="space" desc="最近页面更新于 8 小时前"/>
+      </div>
     </div>
   </div>
 </template>
@@ -33,29 +37,31 @@ import http from "../../utils/http";
 export default {
   data() {
     return {
-      projects: [{uuid: 'none'}],
-      spaces: [{uuid: 'none'}]
+      projects: [],
+      spaces: [],
+      loaded: false
     }
   },
   created () {
     let self = this;
     self.team = self.$route.params.team;
     self.project_list();
-    self.space_list();
   },
   methods: {
-    space_list: function () {
-      let self = this;
-      http.get(self.urls.spaces.format(self.team)).then(function (response) {
-        self.spaces = response.data.spaces;
-      });
-    },
     project_list: function() {
       let self = this;
       let url = self.urls.team_project_list.format(self.team);
       let params = { category: 2, name: "" }
       http.post(url, params).then(function (response) {
         self.projects = response.data;
+        self.space_list();
+      });
+    },
+    space_list: function () {
+      let self = this;
+      http.get(self.urls.spaces.format(self.team)).then(function (response) {
+        self.spaces = response.data.spaces;
+        self.loaded = true;
       });
     }
   },
